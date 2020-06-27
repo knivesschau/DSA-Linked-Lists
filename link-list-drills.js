6a11f985ee83b52170d913c7f08ca352c3bc96ab
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-06-26 18:07:59 -0700

    completed all drills

--- link-list-drills.js
+++ link-list-drills.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+// Linked List Drills 
 class _Node {
     constructor(value, next) {
         this.value = value;
@@ -224,4 +226,93 @@ function findLast(list) {
         currNode = currNode.next;
     }
     return prevNode;
+}
+
+//Mystery Program Drills
+function listReverser(list) {
+    let midNode = list.head;
+    let firstNode = list.head;
+    let lastNode = list.head;
+
+    while (midNode !== null) {
+        firstNode = firstNode.next;
+        midNode = lastNode;
+        lastNode = midNode;
+        midNode = firstNode;
+    }
+    list.head = lastNode;
+    return lastNode; 
+}
+
+function findThird(list) {
+    let currNode = list.head;
+    let prevNode = list.head;
+
+    while (currNode.next.next.next !== null) {
+        prevNode = currNode;
+        currNode = currNode.next;
+    }
+    return currNode;
+}
+
+function findMid(list) {
+    let currNode = list.head;
+    let prevNode = list.head;
+    let listLength = 0;
+    let index = 0;
+
+    while (currNode !== null) {
+        listLength ++;
+        prevNode = currNode;
+        currNode = currNode.next;
+    }
+    let middle = (Math.ceil(listLength / 2) - 1)
+    currNode = list.head;
+    prevNode = list.head;
+
+    while (index < middle) {
+        prevNode = currNode;
+        currNode = currNode.next;
+        index ++
+    }
+    return currNode; 
+}
+
+function cycleList(list) {
+    let item1 = list.head;
+    let item2 = list.head;
+
+    while (item1 !== item2) {
+        item1 = item1.next;
+        item2 = item2.next.next;
+    }
+    if (item1 === item2) {
+        return true;
+    }
+    return false;
+}
+
+function listSort(list) {
+    let currNode = list.head;
+    let head = list.head;
+    let store;
+    let sorting = true; 
+
+    while (sorting) {
+        sorting = false;
+
+        while (currNode.next !== null) {
+            if (currNode.value > currNode.next.value) {
+                store = currNode.value;
+                currNode.value = currNode.next.value;
+                currNode.next.value = store;
+                sorting = true;
+            }
+            currNode = currNode.next;
+        }
+        if (!currNode.next) {
+            currNode = list.head;
+        }
+        return list;
+    }
 }
